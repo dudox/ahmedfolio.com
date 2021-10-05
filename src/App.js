be38e951a01b86a734b6a892740a1f3d455aa5e7
dudox <trybemark@gmail.com>
2021-10-05 13:10:54 +0100

    added toastr css import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Routes from "./router/Routes";
 import ScrollToTop from "./components/ScrollToTop";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
