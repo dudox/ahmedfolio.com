11f84d34f280365428e70176b87a738f04dc8ffa
dudox <snub.com@gmail.com>
2021-10-14 09:53:05 +0100

    added the function to change meta description content for each modals

--- src/components/useDocumentMeta.js
+++ src/components/useDocumentMeta.js
@@ -1,18 +1,23 @@
 // useDocumentTitle.js
 import { useRef, useEffect } from "react";
 
-function useDocumentMeta(title, prevailOnUnmount = false) {
+function useDocumentMeta(title, description, prevailOnUnmount = false) {
   const defaultTitle = useRef(document.title);
+  const defaultDescription = useRef(document.querySelector("meta[name='description']").getAttribute("content"));
+
 
   useEffect(() => {
     document.title = title;
-    
-  }, [title]);
+    document.querySelector("meta[name='description']").setAttribute('content', description);
+
+  }, [title, description]);
 
   useEffect(
     () => () => {
       if (!prevailOnUnmount) {
         document.title = defaultTitle.current;
+        document.querySelector("meta[name='description']").setAttribute('content', defaultDescription.current);
+
       }
     },
     // []
